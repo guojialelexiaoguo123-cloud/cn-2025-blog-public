da01d925207627b3afb2c34a9c36b3a80defe81f
guojialelexiaoguo123-cloud <guojialelexiaoguo123@gmail.com>
2026-05-03 20:21:00 +0800

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -456,7 +456,7 @@ export default function BlogPage() {
 							animate={{ opacity: 1, scale: 1 }}
 							whileHover={{ scale: 1.05 }}
 							whileTap={{ scale: 0.95 }}
-							href='https://github.com/guojialelexiaoguo123-cloud'
+							href='https://juejin.cn/user/3282520391550172'
 							target='_blank'
 							className='card text-secondary static inline-flex items-center gap-2 rounded-xl px-4 py-2 text-xs'>
 							<JuejinSVG className='h-4 w-4' />
