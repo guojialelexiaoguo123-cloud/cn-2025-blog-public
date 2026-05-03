b39131943ff1db016e1420cab1544c936a0bdae5
guojialelexiaoguo123-cloud <guojialelexiaoguo123@gmail.com>
2026-05-03 20:21:04 +0800

    Update music-card.tsx

--- src/components/music-card.tsx
+++ src/components/music-card.tsx
@@ -158,7 +158,7 @@ export default function MusicCard() {
 				<MusicSVG className='h-8 w-8' />
 
 				<div className='flex-1'>
-					<div className='text-secondary text-sm'>白鹭归庭（神里绫华</div>
+					<div className='text-secondary text-sm'>白鹭归庭</div>
 
 					<div className='mt-1 h-2 rounded-full bg-white/60'>
 						<div className='bg-linear h-full rounded-full transition-all duration-300' style={{ width: `${progress}%` }} />
